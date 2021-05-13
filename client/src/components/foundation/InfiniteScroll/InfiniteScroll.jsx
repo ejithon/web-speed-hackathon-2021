9c56851a77c8c2615e844609c9e57a744897fd09
ejithon <yuki8g0@gmail.com>
2021-05-13 19:48:22 +0900

    Fix: dupliate handler of scroll/wheel/touchmove/resize 🐛

--- client/src/components/foundation/InfiniteScroll/InfiniteScroll.jsx
+++ client/src/components/foundation/InfiniteScroll/InfiniteScroll.jsx
@@ -12,10 +12,9 @@ const InfiniteScroll = ({ children, fetchMore }) => {
 
   React.useEffect(() => {
     const handler = () => {
-      // 念の為 2の18乗 回、最下部かどうかを確認する
-      const hasReached = Array.from(Array(2 ** 18), () => {
+      const hasReached = () => {
         return window.innerHeight + Math.ceil(window.scrollY) >= document.body.offsetHeight;
-      }).every(Boolean);
+      };
 
       // 画面最下部にスクロールしたタイミングで、登録したハンドラを呼び出す
       if (hasReached && !prevReachedRef.current) {
@@ -25,14 +24,8 @@ const InfiniteScroll = ({ children, fetchMore }) => {
       prevReachedRef.current = hasReached;
     };
 
-    document.addEventListener('wheel', handler, { passive: false });
-    document.addEventListener('touchmove', handler, { passive: false });
-    document.addEventListener('resize', handler, { passive: false });
     document.addEventListener('scroll', handler, { passive: false });
     return () => {
-      document.removeEventListener('wheel', handler);
-      document.removeEventListener('touchmove', handler);
-      document.removeEventListener('resize', handler);
       document.removeEventListener('scroll', handler);
     };
   }, [fetchMore]);
